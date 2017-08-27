63525340dec7a3af9c73bad036d9cbebd2d0e146
Tiago Justino <tiago.vmj@gmail.com>
2017-08-26 22:53:55 -0300

    first commit

---
+++ angular/assignments/basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+  message = "This is a success!";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
