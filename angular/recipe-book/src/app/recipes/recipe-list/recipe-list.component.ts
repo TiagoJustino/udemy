91fc2a423a7153e63b9dbe9ea93d8a288bc2c125
Tiago Justino <tiago.vmj@gmail.com>
2017-11-02 20:54:36 -0200

    Lesson 5 lecture 58

--- angular/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://photos.smugmug.com/Thai-Recipes-2017/i-cbT2RpB/0/f743dc95/X3/thai-cashew-chicken-recipe-30-X3.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
